290ffbc05c28b6779db7caeef2841bac26699db2
cqm <qmchen2011@126.com>
2013-06-06 11:20:09 +0200

    update main_scene.py, all related to director removed

--- client/main_scene.py
+++ client/main_scene.py
@@ -5,11 +5,9 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import cocos
 class MainScene(cocos.scene.Scene):
 	def __init__(self, sock):
-		super(MainScene, self).__init__(Room(self,sock))
-		self.director = cocos.director.Director()
-		self.director.init()
-		self.director.run(self)
-	
+		super(MainScene, self).__init__(
+			Room(self,sock))
+
 	def addChild(self, child, z, name):
 		self.add(child, z=z, name=name)
 
